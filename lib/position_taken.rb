db6ded07dd30a4f80b86a86d85f232a1db3492b9
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-01-25 20:50:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,8 +3,8 @@ def position_taken?(board, index)
 end 
 
 def position_taken?(board,index)
- if {board[index] == " " || board[index] == "" ||
-      board[index] == nil}
+ if (board[index] == " " || board[index] == "" ||
+      board[index] == nil)
    false
  else
   true
